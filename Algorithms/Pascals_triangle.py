b50513bcbcc8c71913da9585ad60899f4af01acd
Bigtoyka <153129647+Bigtoyka@users.noreply.github.com>
2024-03-11 22:37:43 +0300

    +

--- Algorithms/Pascals_triangle.py
+++ Algorithms/Pascals_triangle.py
@@ -3,7 +3,7 @@ trg = []
 for i in range(n+1):
     trg.append([1]+[0]*n)
 for i in range(1,i+1): # i + 1 чтобы доходить до главной диаганали и не проходить по нулям
-    for j in range(1,n+1):
+    for j in range(1,i+1):
         trg[i][j] = trg[i-1][j]+trg[i-1][j-1]
         print(trg[i][j], end=' ')
     print()
