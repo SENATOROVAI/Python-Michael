e4376c7edb1a9d19494002181dbeefa9b23a43a7
Bigtoyka <153129647+Bigtoyka@users.noreply.github.com>
2024-04-01 23:47:26 +0300

    +

---
+++ Algorithms/Быстрая сортировка.py
@@ -0,0 +1,10 @@
+# функция quick_sort должна выполнить сортировку
+def quick_sort(s):
+    if len(s) <= 1:
+        return s
+    elem = s[0]
+    left = list(filter(lambda x: x< elem,s))
+    center = [i for i in s if i == elem]
+    right = list(filter(lambda x: x> elem,s))
+
+    return quick_sort(left) + center + quick_sort(right)
