c810e2fd2e512007753f77910d9961dfb01f7f83
Bigtoyka <153129647+Bigtoyka@users.noreply.github.com>
2024-04-02 22:53:29 +0300

    +

--- Algorithms/Быстрая сортировка.py
+++ Algorithms/Быстрая сортировка.py
@@ -3,8 +3,8 @@ def quick_sort(s):
     if len(s) <= 1:
         return s
     elem = s[0]
-    left = list(filter(lambda x: x< elem,s))
+    left = list(filter(lambda x: x < elem, s))
     center = [i for i in s if i == elem]
-    right = list(filter(lambda x: x> elem,s))
+    right = list(filter(lambda x: x > elem, s))
 
     return quick_sort(left) + center + quick_sort(right)
--- Algorithms/Сортировка слиянием.py
+++ Algorithms/Сортировка слиянием.py
@@ -2,24 +2,25 @@
 def merge_two_list(a, b):
     c = []
     i = j = 0
-    while i <len(a) and j<len(b):
-        if a[i]<b[j]:
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
             c.append(a[i])
             i += 1
         else:
             c.append(b[j])
             j += 1
-    if i <len(a):
+    if i < len(a):
         c += a[i:]
-    if j <len(b):
+    if j < len(b):
         c += b[j:]
-    return c 
+    return c
+
 
 # функция merge_sort должна выполнить сортировку
 def merge_sort(s):
     if len(s) == 1:
         return s
-    middle = len(s)//2
+    middle = len(s) // 2
     left = merge_sort(s[:middle])
     right = merge_sort(s[middle:])
-    return merge_two_list(left,right)
+    return merge_two_list(left, right)
